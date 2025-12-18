1260cb237f15474a6760f56f5ddf2bb3df81c537
ishtiaqa1 <ishtiaqa2003@gmail.com>
2025-12-18 09:52:57 -0500

    Done

--- src/1-media.js
+++ src/1-media.js
@@ -1,23 +1,66 @@
 // Problem 1: Inheritance and Polymorphism - Media Player
 // Implement your MediaItem, Song, Podcast, and Audiobook classes below
 class MediaItem {
+    constructor(title, duration) {
+        this.title = title;
+        this.duration = duration;
+    }
 
+    play() {
+        return `Playing: ${this.title}`
+    }
+
+    getFormattedDuration() {
+        return `${Math.floor(this.duration/60)}:${(this.duration%60).toString().padStart(2, '0')}`;
+    }
 }
 
-class Song {
+class Song extends MediaItem {
+    constructor(title, duration, artist) {
+        super(title, duration);
+        this.artist = artist;
+    }
 
+    play() {
+        return `${super.play()} by ${this.artist}`;
+    }
 }
 
-class Podcast {
+class Podcast extends MediaItem {
+    constructor(title, duration, host, episodeNumber) {
+        super(title, duration);
+        this.host = host;
+        this.episodeNumber = episodeNumber;
+    }
 
+    play() {
+        return `${super.play()} with host ${this.host}, Episode ${this.episodeNumber}`;
+    }
 }
 
-class Audiobook {
+class Audiobook extends MediaItem {
+    constructor(title, duration, author, narrator) {
+        super(title, duration);
+        this.author = author;
+        this.narrator = narrator;
+    }
 
+    play() {
+        return `${super.play()} by ${this.author}, narrated by ${this.narrator}`;
+    }
 }
 
 const test = () => {
+  const song = new Song("Bohemian Rhapsody", 354, "Queen");
+  const podcast = new Podcast("Tech Talk", 2400, "Jane Smith", 42);
+  const audiobook = new Audiobook("The Great Gatsby", 32400, "F. Scott Fitzgerald", "Jake Gyllenhaal");
 
+  const mediaLibrary = [song, podcast, audiobook];
+
+  // Polymorphism: same method call, different behavior
+  mediaLibrary.forEach(media => {
+    console.log(media.play());
+  });
 }
 
 module.exports = { MediaItem, Song, Podcast, Audiobook };
--- src/2-team.js
+++ src/2-team.js
@@ -2,11 +2,56 @@
 // Implement your Player and Team classes below
 
 class Player {
-
+    constructor(name, position, jerseyNumber) {
+        this.name = name;
+        this.position = position;
+        this.jerseyNumber = jerseyNumber;
+    }
 }
 
 class Team {
+    #starters = [];
+    #bench = [];
 
+    constructor(name) {
+        this.name = name;
+    }
+
+    getStarters() {
+        return this.#starters;
+    }
+
+    getBench() {
+        return this.#bench;
+    }
+
+    addBenchPlayer(player) {
+        this.#bench.push(player);
+    }
+
+    getPlayerCount() {
+        return this.#bench.length + this.#starters.length;
+    }
+
+    moveToBench(name) {
+        const player = this.#starters.findIndex((starter) => starter.name === name);
+        if (player != -1) {
+            const removed = this.#starters.splice(player,1);
+            this.#bench.push(removed[0]);
+            return true;
+        }
+        return false;
+    }
+
+    moveToStarters(name) {
+        const player = this.#bench.findIndex((bench) => bench.name === name);
+        if (player != -1) {
+            const removed = this.#bench.splice(player,1);
+            this.#starters.push(removed[0]);
+            return true;
+        }
+        return false;
+    }
 }
 
 module.exports = { Player, Team };
